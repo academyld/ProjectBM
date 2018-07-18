f5307b89dab7625544e23afaca29203072740fa7
academyld <41340334+academyld@users.noreply.github.com>
2018-07-18 16:31:38 -0400

    Updated
    Made earnedPoints a JSON String

--- transfer_code.js
+++ transfer_code.js
@@ -1,43 +1,23 @@
+//Place at the start of document
+var gitToken = "token <TOKEN GOES HERE>"; //GitHub Access Token
+
+==============================================
+ //Place inside function Script#()
   var player = GetPlayer();
-  // var earnedPoints=player.GetVar("points"); //remove the comment marks
-  var earnedPoints = 100; // for testing
+  var earnedPoints=player.GetVar("points"); //remove the comment marks
+  //var earnedPoints = 25; // for testing
+  var stringPnts = String(earnedPoints)
+  var gitContent = {"description": "ProjectBM Points","public": true,"files": {"bmpoints.xml": {"content": stringPnts}}};
+  var contJSON = JSON.stringify(gitContent); 
 
-if (earnedPoints < 50) {     
 	$.ajax({ 
     		url: 'https://api.github.com/gists/d3c9f53144ed69f8fe2b1785fb740f11',
    	 	type: 'PATCH',
     		beforeSend: function(xhr) { 
-        		xhr.setRequestHeader("Authorization", "token <TOKEN>"); 
+        		xhr.setRequestHeader("Authorization", gitToken); 
     			       },
-    		data: '{"description": "ProjectBM Points","public": true,"files": {"bmpoints.xml": {"content": "100"}}}',
+    		data: contJSON,
    		success: function(response) {
 			console.log(response, earnedPoints,);
 				},
        		});
-} else if (earnedPoints < 100) {
-	$.ajax({ 
-    		url: 'https://api.github.com/gists/d3c9f53144ed69f8fe2b1785fb740f11',
-   	 	type: 'PATCH',
-    		beforeSend: function(xhr) { 
-        		xhr.setRequestHeader("Authorization", "token <TOKEN>"); 
-    			       },
-    		data: '{"description": "ProjectBM Points","public": true,"files": {"bmpoints.xml": {"content": "150"}}}',
-   		success: function(response) {
-			console.log(response, earnedPoints,);
-				},
-       		});
-
-} else {
-	$.ajax({ 
-    		url: 'https://api.github.com/gists/d3c9f53144ed69f8fe2b1785fb740f11',
-   	 	type: 'PATCH',
-    		beforeSend: function(xhr) { 
-        		xhr.setRequestHeader("Authorization", "token <TOKEN>"); 
-    			       },
-    		data: '{"description": "ProjectBM Points","public": true,"files": {"bmpoints.xml": {"content": "200"}}}',
-   		success: function(response) {
-			console.log(response, earnedPoints,);
-				},
-       		});
-
-	}
